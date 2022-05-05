f6a0c198b9baef244b0ba529fe1e8a3525077808
Michael Watson <xambrew@gmail.com>
2022-05-05 11:21:32 -0700

    Add try/catch+comment for REST API generation

--- src/graphql/bar.ts
+++ src/graphql/bar.ts
@@ -3,9 +3,15 @@ import { Resolvers } from '../__generated__/resolvers-types';
 export const resolvers: Resolvers = {
   Query: {
     async bar(_parent, { id }, { dataSources }) {
-      const bar = await dataSources.BarAPI.bars.showBarById(id);
-      console.log(dataSources);
-      return bar.data;
+      try {
+        const bar = await dataSources.BarAPI.bars.showBarById(id);
+        return bar.data;
+      } catch (err: any) {
+        //We don't actually have the REST API running locally...
+        //  just an example of using a REST API as a datasource
+        console.log(err.message);
+        return { name: 'Bar' };
+      }
     },
   },
   Bar: {
